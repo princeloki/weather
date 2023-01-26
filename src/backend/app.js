7eaf1c13d79811d53781f268f343a8ad22d2067a
princeloki <teriqstrachan@yahoo.com>
2023-01-26 15:34:40 +0900

    Change the port information

--- src/backend/app.js
+++ src/backend/app.js
@@ -36,6 +36,6 @@ app.post('/current', async (req, res) => {
     }
 })
 
-app.listen(8000, () => {
-    console.log('listening on port 8000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on port 3000');
 })
