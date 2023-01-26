d2935c2ae11d198399cb0a19be201beb36d88436
princeloki <teriqstrachan@yahoo.com>
2023-01-26 17:56:22 +0900

    made the page more responsive with media queries

--- src/view/javascript/script.js
+++ src/view/javascript/script.js
@@ -1,11 +1,14 @@
 const nav = document.querySelector("#nav");
+const forecast = document.querySelector(".forecast");
 const burger = document.querySelector(".burger");
 
 burger.addEventListener("click", () => {
     if(nav.classList.contains("invisible")){
         nav.classList.remove("invisible");
+        forecast.classList.add("invisible2");
     } else{
         nav.classList.add("invisible");
+        forecast.classList.remove("invisible2");
     }
 });
 
